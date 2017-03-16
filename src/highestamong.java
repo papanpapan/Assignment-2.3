9c127a2d3e1967a0ff61313fe0f6172f5e6f0785
papanpapan <samim.skmolla@gmail.com>
2017-03-16 15:05:47 +0530

    highestamong
    highestamong

---
+++ src/highestamong.java
@@ -0,0 +1,21 @@
+
+public class highestamong {
+
+	
+	public static void main(String[] args) {
+		int a=97,b=98,c=99,max=0;
+		
+		 max=a;
+		 if(b>max){
+			max=b;
+		}
+			if(c>max){
+				max=c;
+			}
+			System.out.println("higest number is max:"+max);
+
+	}
+
+	}
+
+
